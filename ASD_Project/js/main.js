ba0d8e528ae62b2b2eea51338763570cf5359496
Jordan Weaver <weaver.jordan6@gmail.com>
2013-11-06 00:04:13 -0600

    Update

--- ASD_Project/js/main.js
+++ ASD_Project/js/main.js
@@ -10,7 +10,6 @@ $('#signupform').on("pageinit",function(){
 $('#submit').on('click', function(){
 	console.log('Click Event works');
 
-function storeData(key){
 		console.log('In Store Data');
 		 if($('#key').val() == ""){
 			var key	= Math.floor(Math.random()*10000001);
@@ -19,28 +18,34 @@ function storeData(key){
 		};
 		
 		var item				= {};
-			item.fname			=["First Name", $('#firstname').value];
-			item.lname			=["Last Name", $('#lastname').value];
-			item.state			=["State", $('#usstate').value];
-			item.email			=["Email", $('#email').value];
+			item.fname			=["First Name", $('#firstname').val()];
+			item.lname			=["Last Name", $('#lastname').val()];
+			item.state			=["State", $('#usstate').val()];
+			item.email			=["Email", $('#email').val()];
 			
 			console.log('#key');
 			//Save Data into Local Storage:
 			localStorage.setItem(key, JSON.stringify(item));
 			window.reload(5);
 			
-		
-}
+			
+			var firstName = localStorage.getItem('fname');
+			firstName = "<strong>First Name:</strong>" + firstName;
+			$("#display ul").append('<li>' + firstName + '</li>');
+			
+			var lastName = localStorage.getItem('lname');
+			lastName = "<strong>Last Name:</strong>" + lastName;
+			$(".display ul").append('<li>' + lastName + '</li>');
+			
+			var state = localStorage.getItem('state');
+			state = "<strong>State:</strong>" + state;
+			$(".display ul").append('<li>' + state + '</li>');
+			
+			var email = localStorage.getItem('email');
+			email = "<strong>Email:</strong>" + email;
+			$(".display ul").append('<li>' + email + '</li>');
+			
+			console.log(firstName + lastName + state + email);
+
 });
 
-
-//	function loadData(){
-	//	$('#fname').val(localStorage.fname);
-	//}
-	
-//	function saveData(){
-//	localStorage.fname = $('#fname');
-	//	localStorage.lname = $('#lname').val();
-	//	localStorage.state = $('state');
-	//	localStorage.email = $('email');
-	//}
---
+++ ASD_Project/xhr/json.js
@@ -0,0 +1,28 @@
+<!--json-->
+
+{
+
+	"Data":[
+            {
+                    "First Name": "Jordan",
+                    "Last Name": "Weaver",
+                    "Address": "555 W 5th St.",
+                    "City": "Blue Earth",
+                    "State": "Minnesota",
+                    "Email": "jweav6@fullsail.edu"
+                    
+               },
+        
+            {
+                    "First Name": "Adam",
+                    "Last Name": "Zwanzinger",
+                    "Address": "556 W 6th St.",
+                    "City": "Klemme",
+                    "State": "Iowa",
+                    "Email": "azinger@gmail.com"
+                    
+               }
+]}
+
+	
+	
