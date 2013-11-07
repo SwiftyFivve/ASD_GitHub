030209d81a2f5854417a467a1b14de57e8b1a34f
Jordan Weaver <weaver.jordan6@gmail.com>
2013-11-07 16:42:43 -0600

    Updated

--- ASD_Project/js/main.js
+++ ASD_Project/js/main.js
@@ -7,6 +7,39 @@ $('#signupform').on("pageinit",function(){
 	});		
 
 
+
+var autoFillData = function (){
+                for(var n in json){
+                        var id = Math.floor(Math.random()*1000000);
+                        localStorage.setItem(id, JSON.stringify(json[n]));
+				}
+};
+
+
+
+	function toggleControls(n){
+		switch(n){
+			case "on":
+				$('#signupform').style.display = "none";
+				$('#clear').style.display =  "inline";
+				$('#displayLink').style.display = "none";
+				$('#addNew').style.display = "inline";
+				break
+			case "off":
+				$('#signupform').style.display = "block";
+				$('#clear').style.display =  "inline";
+				$('#displayLink').style.display = "inline";
+				$('#addNew').style.display = "none";
+				$('#items').style.display = "none";
+				break;
+			default:
+				return false;	
+		}
+	}
+
+
+
+
 $('#submit').on('click', function(){
 	console.log('Click Event works');
 
@@ -23,29 +56,34 @@ $('#submit').on('click', function(){
 			item.state			=["State", $('#usstate').val()];
 			item.email			=["Email", $('#email').val()];
 			
-			console.log('#key');
 			//Save Data into Local Storage:
 			localStorage.setItem(key, JSON.stringify(item));
 			window.reload(5);
 			
-			
-			var firstName = localStorage.getItem('fname');
-			firstName = "<strong>First Name:</strong>" + firstName;
-			$("#display ul").append('<li>' + firstName + '</li>');
-			
-			var lastName = localStorage.getItem('lname');
-			lastName = "<strong>Last Name:</strong>" + lastName;
-			$(".display ul").append('<li>' + lastName + '</li>');
-			
-			var state = localStorage.getItem('state');
-			state = "<strong>State:</strong>" + state;
-			$(".display ul").append('<li>' + state + '</li>');
-			
-			var email = localStorage.getItem('email');
-			email = "<strong>Email:</strong>" + email;
-			$(".display ul").append('<li>' + email + '</li>');
-			
-			console.log(firstName + lastName + state + email);
+			console.log(localStorage.getItem(key));
+			console.log(localStorage.getItem(fname));
+		
+	var getData = function(){
+		toggleControls("on");
+		if(localStorage.length === 0){
+			alert("There is no data in Local Storage, so default data was added.");
+			console.log(getData);
+			autoFillData();
 
+		};
+		
+		$("#items").remove();
+		$("#display")
+			.html('div><ul></ul></div>')
+			.find("div:first")
+			.attr("id", "itms")
+			.css("display", "block")
+			.end()
+			.find("ul:first")
+			.attr("id", "itmesList");
+		for(var i=0, len=localStorage.length; i<len; i++){
+			console.log(len);
+		};
+};
 });
 
