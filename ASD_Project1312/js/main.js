d40fa6661e2d3fb7a0a43c55552e95b324023161
Jordan Weaver <weaver.jordan6@gmail.com>
2013-12-04 17:58:07 -0600

    Commit

--- ASD_Project1312/js/main.js
+++ ASD_Project1312/js/main.js
@@ -8,14 +8,11 @@
 $('#submit').on('click',function(e){
 	e.preventDefault();
 	
-	
-	if($('#key').val() == ""){
-		var id	= Math.floor(Math.random()*10000001);
-	}else{
-		var id = $('#key').val();
-	};
+	// Date key
+	var key = new Date();
+		console.log(key);
 		
-		
+	//My Object	
 	var myObj = {};
 		myObj.key = key;
 		myObj.fname = $('#firstname').val();
@@ -24,7 +21,7 @@ $('#submit').on('click',function(e){
 		myObj.email = $('#email').val();
 		console.log(myObj);
 		
-		
+	//Display	
 	var html = '';
 		html +='<li id"' + myObj.key + '">';
 		html += '<ul>';
@@ -33,12 +30,13 @@ $('#submit').on('click',function(e){
 		html += '<li>' + myObj.state + '</li>';
 		html += '<li>' + myObj.email + '</li>';
 		html += '<li><a href="#" class="edit">Edit</a></li>';
+		html += '<li><a href="#" class="delete">Delete</a></li>';
 		html += '</ul></li>';
 		
 		$('#display ul#main-list').append(html);
 		
 		
-		
+	// Key	
 	if($('#key').val() == ""){
 		var id	= Math.floor(Math.random()*10000001);
 	}else{
@@ -54,28 +52,62 @@ $('#submit').on('click',function(e){
 		//Save Data into Local Storage:
 		localStorage.setItem(id, JSON.stringify(item));
 		
-	$('.edit').on('click',function(e){
-		e.preventDefault();
-		
-		
-		//saveData(currKey);
-		 var currKey = $(this).data(key);
-				console.log(currKey);
-                var value = localStorage.getItem(currKey);
-                console.log(value);
-				var item = JSON.parse(value);
-					
-				
-				
-				
-				editKey = key;
-                $("#submit").data("key",editKey);
-                console.log($("#submit").data("key"));
-				
-		
-	});
-			
-		
-	
+		$(".delete").on("click", deleteItem);
+		$(".edit").on("click", editItem);
 	
 });
+
+
+
+//Edit
+	var editItem = function(){
+    var key = $(this).data('key');
+		console.log(key);
+    var value = localStorage.getItem(key);
+        console.log(value);
+	var item = JSON.parse(value);
+	};
+					
+				
+					
+//Delete function	
+	var  deleteItem = function (){
+	var ask = confirm("Are you sure you want to delete this contact?");
+     	if(ask){
+             localStorage.removeItem($(this).parent().attr("data-key"));
+             alert("Contact was deleted!");
+             window.location.reload();
+     	}else{
+             alert("Contact was not deleted.");
+     	}
+	};
+
+	
+//JSON AJAX	
+$('.json').on('click', function(){
+	//e.preventDefault();
+	console.log('In json click event');
+	
+	$.ajax({
+		url: "xhr/data.JSON",
+		type: "GET",
+		dataType:"json",
+		success: function(result){
+			console.log(result)
+		},
+		error: function (error, parseerror) { 
+		console.log(error, parseerror) 
+		}
+	});
+	
+	var myObj = {};
+		myObj.key = key;
+		myObj.fname = $('#firstname').val();
+		myObj.lname = $('#lastname').val();
+		myObj.state = $('#usstate').val();
+		myObj.email = $('#email').val();
+		console.log(myObj);
+	
+});
+	
+	
